b950453576d791b49f863554ff84dcc4426ed3a2
M01i <1061336266@qq.com>
2020-03-17 16:26:38 +0800

    1.0

--- src/components/home/reducer.js
+++ src/components/home/reducer.js
@@ -12,17 +12,17 @@ export default (state = initialState, { type, p,x,y,z}) => {
     switch (type) {
         case 'GET':
             var newState={...state};
-            newState.visible=true;
             newState.title=p;
             newState.goods=x;
             newState.bottom=y;
             newState.banner=z;
             newState.loading=false;
+            newState.visible=true;
             return newState;
         case 'CHANGE':
             var newState={...state};
-            newState.visible=false;
             newState.goodslist=p;
+            newState.visible=false;
             console.log(p)
             return newState;
         case 'L-START':
